88e01bdc9bcbbd340e7919cda8d0c60ec908cfbe
Victor <victorkling.p@gmail.com>
2025-10-05 20:56:58 -0300

    firts commit

---
+++ schemaTypes/home.ts
@@ -0,0 +1,10 @@
+export default {
+  name: 'home',
+  type: 'document',
+  title: 'Página Home',
+  fields: [
+    { name: 'title', type: 'string', title: 'Título Principal' },
+    { name: 'subtitle', type: 'string', title: 'Subtítulo' },
+    { name: 'banner', type: 'image', title: 'Imagem de destaque' },
+  ],
+};
